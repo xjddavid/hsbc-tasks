1ed2242903f5080b6e27253b3a792d2f6c696b2a
xujiang <xjddavid@gmail.com>
2019-07-25 19:47:39 +0800

    handle the wrong request exception

--- src/main/java/com/jiang/tasks/CustomExceptionHandler.java
+++ src/main/java/com/jiang/tasks/CustomExceptionHandler.java
@@ -1,18 +1,27 @@
 package com.jiang.tasks;
 
 import org.springframework.http.HttpStatus;
+import org.springframework.http.converter.HttpMessageNotReadableException;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.ResponseStatus;
 
 @ControllerAdvice
-public class TaskNotFoundAdvice {
+public class CustomExceptionHandler {
 
     @ResponseBody
     @ExceptionHandler(TaskNotFoundException.class)
     @ResponseStatus(HttpStatus.NOT_FOUND)
     String taskNotFoundHandler(TaskNotFoundException ex) {
         return ex.getMessage();
+    }
+
+    @ResponseBody
+    @ExceptionHandler(HttpMessageNotReadableException.class)
+    @ResponseStatus(HttpStatus.BAD_REQUEST)
+    String badRequestHandler(HttpMessageNotReadableException ex) {
+        ex.printStackTrace();
+        return "Not valid request";
     }
 }
