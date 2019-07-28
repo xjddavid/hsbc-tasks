db8e44b9b2849ebc4fba0b439a1c01424ddf2221
xujiang <xjddavid@gmail.com>
2019-07-28 19:34:20 +0800

    remove useless allconstructor

--- app/src/main/java/com/jiang/tasks/dto/TaskCreateDto.java
+++ app/src/main/java/com/jiang/tasks/dto/TaskCreateDto.java
@@ -1,10 +1,8 @@
 package com.jiang.tasks.dto;
 
-import lombok.AllArgsConstructor;
 import lombok.Data;
 
 @Data
-@AllArgsConstructor
 public class TaskCreateDto {
     private String title;
     private String dueDate;
